20e467ba00e64be8ee4d097bf1ac6ebb616b726d
Matius Kristyanto Mulyono <matius.km@gmail.com>
2024-05-31 23:48:51 +0700

    hide some data from category object

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,7 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+    protected $hidden = ['created_at', 'updated_at'];
 
     public function products(): HasMany
     {
